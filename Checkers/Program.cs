46d51fbb8a2bb0e9c8de1912db0de8d922d6b015
standthis <standthis@protonmail.ch>
2018-04-13 17:15:39 +0200

    Create class stubs

--- Checkers/Program.cs
+++ Checkers/Program.cs
@@ -35,6 +35,34 @@ namespace Checkers
     }
     public class IllegalMoveException : ApplicationException { }
 
+    public class Board : IBoard
+    {
+        public void Move(IPiece piece, int destination)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPiece Occupant(int position)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<int> Pieces(Color color)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Promote(IPiece piece)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveCapturedPieces()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    
     class Program
     {
         static void Main(string[] args)
