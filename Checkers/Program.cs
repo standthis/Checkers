04cb2b4d0912196858322da73f16a0a1cfd2d5e5
standthis <standthis@protonmail.ch>
2018-04-14 11:59:02 +0200

    Pass test

--- Checkers/Program.cs
+++ Checkers/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Checkers
 {
@@ -37,17 +38,30 @@ namespace Checkers
 
     public class Board : IBoard
     {
+        Piece[] white, black;
+        private Piece[] PiecesWithPositions(Color c, params int[] positions)
+        {
+           return positions.Select(v => new Piece(c,v)).ToArray(); 
+        }
+        public Board()
+        {
+            white = PiecesWithPositions(Color.Black, 1,2,3,4,5,6,7,8,9,10,11,12);
+            black = PiecesWithPositions(Color.White,21,22,23,24,25,26,27,28,29,30,31,32);
+        }
+
+        public IEnumerable<int> Pieces(Color color)
+        {
+            if (color == Color.White){
+                return white.Select(x => x.Position);
+            }
+            return black.Select(x => x.Position);
+        }
         public void Move(IPiece piece, int destination)
         {
             throw new NotImplementedException();
         }
 
         public IPiece Occupant(int position)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<int> Pieces(Color color)
         {
             throw new NotImplementedException();
         }
@@ -63,36 +77,48 @@ namespace Checkers
         }
     }
 
-    public class King : IPiece
+    public class Piece : IPiece
     {
-        public Status Status => throw new NotImplementedException();
+        public Piece(Color c, int pos)
+        {
+            Status = Status.Active;
+            Position = pos;
+            Color = c;
+        }
+        public virtual Status Status { get; set; }
+        public virtual Color Color { get; private set; }
+        public virtual int Position { get; private set; }
+        //public virtual Status Status => throw new NotImplementedException();
 
-        public Color Color => throw new NotImplementedException();
+        //public virtual Color Color => throw new NotImplementedException();
 
-        public int Position => throw new NotImplementedException();
+        //public virtual int Position => throw new NotImplementedException();
 
-        public IEnumerable<int> CapturingMoves(IBoard board)
+        public virtual IEnumerable<int> CapturingMoves(IBoard board)
         {
             throw new NotImplementedException();
         }
 
-        public void Move(int destination)
+        public virtual void Move(int destination)
         {
             throw new NotImplementedException();
         }
 
-        public IEnumerable<int> NormalMoves(IBoard board)
+        public virtual IEnumerable<int> NormalMoves(IBoard board)
         {
             throw new NotImplementedException();
         }
+    }
+    public class King : Piece
+    {
+        public King(Color c, int p) : base(c, p){}
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            King KingRichard = new King();
+            Console.WriteLine("Hello World!");    
         }
     }
 }
